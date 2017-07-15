c2965be067db65e65398315d3b66ea3a89e406a5
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2017-07-15 19:54:31 +0200

    Nice dump of method names

--- src/Dumper.cpp
+++ src/Dumper.cpp
@@ -1,14 +1,41 @@
+#include <cassert>
 #include <iostream>
 #include "ClassFile.h"
+
+string getString(ClassFile const& class_, int index)
+{
+  auto& constant = class_.const_pool_[index];
+  assert(constant.tag_ == (int)CONSTANT::Utf8);
+  return string(constant.bytes_.begin(), constant.bytes_.end());
+}
 
 void dumpClass(ClassFile const& class_)
 {
   cout << "Class" << endl;
 
+  if(0)
+  {
+    int i = 0;
+    cout << "Constants:" << endl;
+
+    for(auto& constant : class_.const_pool_)
+    {
+      (void)constant;
+      cout << "[" << i << "] ";
+
+      if(constant.tag_ == (int)CONSTANT::Utf8)
+        cout << constant.bytes_.data();
+
+      cout << endl;
+
+      ++i;
+    }
+  }
+
   for(auto& method : class_.methods_)
   {
     (void)method;
-    cout << " * method" << endl;
+    cout << " * method: " << getString(class_, method.name_index_) << endl;
   }
 
   cout << endl;
