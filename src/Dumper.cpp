43107ec0ef0ffe403c097459973900f347fe87ba
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2017-07-15 23:30:42 +0200

    Improve dump

--- src/Dumper.cpp
+++ src/Dumper.cpp
@@ -16,9 +16,31 @@ string getClass(ClassFile const& class_, int index)
   return getString(class_, constant.name_index_);
 }
 
+const char* TagToString(CONSTANT tag)
+{
+  switch(tag)
+  {
+  case CONSTANT::Utf8:  return "Utf8";
+  case CONSTANT::Integer:  return "Integer";
+  case CONSTANT::Float:  return "Float";
+  case CONSTANT::Long:  return "Long";
+  case CONSTANT::Double:  return "Double";
+  case CONSTANT::Class:  return "Class";
+  case CONSTANT::String:  return "String";
+  case CONSTANT::Fieldref:  return "Fieldref";
+  case CONSTANT::Methodref:  return "Methodref";
+  case CONSTANT::InterfaceMethodref:  return "InterfaceMethodref";
+  case CONSTANT::NameAndType:  return "NameAndType";
+  case CONSTANT::MethodHandle:  return "MethodHandle";
+  case CONSTANT::MethodType:  return "MethodType";
+  case CONSTANT::InvokeDynamic:  return "InvokeDynamic";
+  default: return "?";
+  }
+}
+
 void dumpClass(ClassFile const& class_)
 {
-  if(0)
+  if(1)
   {
     int i = 0;
     cout << "Constants:" << endl;
@@ -26,7 +48,8 @@ void dumpClass(ClassFile const& class_)
     for(auto& constant : class_.const_pool_)
     {
       (void)constant;
-      cout << "[" << i << "] ";
+      cout << "#" << i;
+      cout << " [" << TagToString((CONSTANT)constant.tag_) << "] ";
 
       if(constant.tag_ == (int)CONSTANT::Utf8)
         cout << constant.utf8_;
@@ -39,7 +62,7 @@ void dumpClass(ClassFile const& class_)
 
   cout << "Class: " << getClass(class_, class_.this_class_) << endl;
 
-  if(0)
+  if(1)
   {
     for(auto& method : class_.methods_)
       cout << " * method: " << getString(class_, method.name_index_) << endl;
